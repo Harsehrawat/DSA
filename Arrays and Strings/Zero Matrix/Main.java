ca69175bf63edb0255b056a893e05c3058453327
Harsh Sehrawat <harsehrawat@gmail.com>
2025-07-14 13:32:05 +0530

    correct code

--- Arrays and Strings/Zero Matrix/Main.java
+++ Arrays and Strings/Zero Matrix/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] arr = {
-            {1, 0, 3},
+            {1, 3, 0},
             {4, 5, 6},
             {7, 8, 0}
         };
@@ -13,7 +13,7 @@ public class Main {
         for(int i  =0;i<arr.length;i++){
             for(int j = 0;j<arr[0].length;j++){
                 if(arr[i][j] == 0 && !visited[i][j]){
-                    dfs(arr,i,j,visited);
+                    spreadZero(i, j, arr, visited);
                 }
             }
         }
@@ -28,35 +28,44 @@ public class Main {
         }
 
     }
-    static void dfs(int[][] arr,int i,int j,boolean[][] visited){
-        // expand in all 4 directions 
-        // up
-        int iUp = i;
-        while(iUp >= 0){
-            arr[iUp][j] = 0;
-            visited[iUp][j] = true;
+    static void spreadZero(int i, int j, int[][] arr, boolean[][] visited) {
+        int iUp = i, iDown = i;
+        int jLeft = j, jRight = j;
+    
+        System.out.println("SpreadZero called for i: "+i+", j:"+j);
+        while (iUp >= 0) {
+            if(arr[iUp][j] != 0){
+                arr[iUp][j] = 0;
+                visited[iUp][j] = true;
+            }
             iUp--;
-        };
-
-        int iDown = i;
-        while(iDown < arr.length){
-            arr[iDown][j] = 0;
-            visited[iDown][j] = true;
+        }
+        System.out.println("iUp out of loop at iUp: "+iUp);
+        while (iDown < arr.length) {
+            if(arr[iDown][j] != 0){
+                arr[iDown][j] = 0;
+                visited[iDown][j] = true;
+            }
             iDown++;
         }
-
-        int jLeft = j;
+        System.out.println("iDown out of loop at iDown: "+iDown);
+    
         while (jLeft >= 0) {
-            arr[i][jLeft] = 0;
-            visited[i][jLeft] = true;
+            if (arr[i][jLeft] != 0 ){
+                arr[i][jLeft] = 0;
+                visited[i][jLeft] = true;
+            }
             jLeft--;
         }
+        System.out.println("jLeft out of loop at jLeft: "+jLeft);
 
-        int jRight = j;
-        while(jRight < arr[0].length){
-            arr[i][jRight] = 0;
-            visited[i][jRight] = true;
+        while (jRight < arr[0].length) {
+            if (arr[i][jRight] == 0 ) {
+                arr[i][jRight] = 0;
+                visited[i][jRight] = true;
+            }
             jRight++;
         }
+        System.out.println("jRight out of loop at jRight: "+jRight);
     }
 }
